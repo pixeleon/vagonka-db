a938b61fe24e28ca48c6471de41279968d42fbdd
pixeleon <shevelvaddm@gmail.com>
2020-05-18 12:04:28 +0300

    changed to mariadb heroku connection

--- src/net/vadique/khpi/vagonkadb/conn/MySQLConnection.java
+++ src/net/vadique/khpi/vagonkadb/conn/MySQLConnection.java
@@ -12,22 +12,27 @@ public class MySQLConnection {
 //	     String dbName = "vagonka";
 //	     String userName = "vagonka_user";
 //	     String password = "";
-		String userName = "fsufurjoqlcaqa";
-		String password = "55b88af1fa09ede2386acac435564147de7d776685c44d1b7c21f48bf1adba45";
+		String userName = "zue12pxc9143ue33";
+		String password = "e1zn6qn8z5zd612l";
 	     return getMySQLConnection(userName, password);
 	}
 
 	public static Connection getMySQLConnection(String userName, String password) 
 			throws ClassNotFoundException, SQLException {
-		//Class.forName("com.mysql.cj.jdbc.Driver");
+		Class.forName("com.mysql.cj.jdbc.Driver");
 //		String connectionURL = "jdbc:mysql://"
 //				+ "localhost"
 //				+ "/vagonka"
 //				+ "?noAccessToProcedureBodies=true";
-		Class.forName("org.postgresql.Driver");
-		String connectionURL = "jdbc:postgresql://"
-				+ "ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432"
-				+ "/d62hs3sdk2ccl6"
+//		Class.forName("org.postgresql.Driver");
+//		String connectionURL = "jdbc:postgresql://"
+//				+ "ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432"
+//				+ "/d62hs3sdk2ccl6"
+//				+ "?&sslmode=require"
+//				+ "&noAccessToProcedureBodies=true";
+		String connectionURL = "jdbc:mysql:"
+				+ "//zue12pxc9143ue33:e1zn6qn8z5zd612l@eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306"
+				+ "/l0ojmu0la4w8ro5b"
 				+ "?&sslmode=require"
 				+ "&noAccessToProcedureBodies=true";
 		return DriverManager.getConnection(connectionURL, userName, password);
