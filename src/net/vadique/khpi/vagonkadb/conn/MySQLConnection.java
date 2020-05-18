964ad969d08c62f71d0c209600c1510abd7efe48
pixeleon <shevelvaddm@gmail.com>
2020-05-18 13:08:13 +0300

    removed postgre things

--- src/net/vadique/khpi/vagonkadb/conn/MySQLConnection.java
+++ src/net/vadique/khpi/vagonkadb/conn/MySQLConnection.java
@@ -24,12 +24,6 @@ public class MySQLConnection {
 //				+ "localhost"
 //				+ "/vagonka"
 //				+ "?noAccessToProcedureBodies=true";
-//		Class.forName("org.postgresql.Driver");
-//		String connectionURL = "jdbc:postgresql://"
-//				+ "ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432"
-//				+ "/d62hs3sdk2ccl6"
-//				+ "?&sslmode=require"
-//				+ "&noAccessToProcedureBodies=true";
 		String connectionURL = "jdbc:mysql:"
 				+ "//zue12pxc9143ue33:e1zn6qn8z5zd612l@eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306"
 				+ "/l0ojmu0la4w8ro5b"
