bad4b77c15c7577df6c51735239d160353d27db1
pixeleon <shevelvaddm@gmail.com>
2020-05-18 10:50:02 +0300

    postgresql connection attempt

--- src/net/vadique/khpi/vagonkadb/conn/MySQLConnection.java
+++ src/net/vadique/khpi/vagonkadb/conn/MySQLConnection.java
@@ -8,18 +8,27 @@ public class MySQLConnection {
 	public static Connection getMySQLConnection() 
 			throws ClassNotFoundException, SQLException {
 		
-		 String hostName = "localhost";
-	     String dbName = "vagonka";
-	     String userName = "vagonka_user";
-	     String password = "";
+//		 String hostName = "localhost";
+//	     String dbName = "vagonka";
+//	     String userName = "vagonka_user";
+//	     String password = "";
+		String userName = "fsufurjoqlcaqa";
+		String password = "55b88af1fa09ede2386acac435564147de7d776685c44d1b7c21f48bf1adba45";
 	     return getMySQLConnection(userName, password);
 	}
 
 	public static Connection getMySQLConnection(String userName, String password) 
 			throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		String connectionURL = "jdbc:mysql://localhost/vagonka"
-				+ "?noAccessToProcedureBodies=true";
+		//Class.forName("com.mysql.cj.jdbc.Driver");
+//		String connectionURL = "jdbc:mysql://"
+//				+ "localhost"
+//				+ "/vagonka"
+//				+ "?noAccessToProcedureBodies=true";
+		String connectionURL = "jdbc:postgresgl://"
+				+ "ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432"
+				+ "/d62hs3sdk2ccl6"
+				+ "?noAccessToProcedureBodies=true"
+				+ "&sslmode=require";
 		return DriverManager.getConnection(connectionURL, userName, password);
 	}
 	
