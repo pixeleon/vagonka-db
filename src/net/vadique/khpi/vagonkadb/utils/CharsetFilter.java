2a802c1336175edcc4a4f9eb929c04c9563433cb
pixeleon <shevelvaddm@gmail.com>
2020-05-18 10:31:08 +0300

    another tiny filter fixes

--- src/net/vadique/khpi/vagonkadb/utils/CharsetFilter.java
+++ src/net/vadique/khpi/vagonkadb/utils/CharsetFilter.java
@@ -10,35 +10,30 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 public class CharsetFilter implements Filter {
-	
+
 	private String encoding;
-	
+
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		encoding = filterConfig.getInitParameter("requestEncoding");
-        if (encoding == null) encoding = "UTF-8";
+		if (encoding == null)
+			encoding = "UTF-8";
 	}
-
-
 
 	@Override
 	public void destroy() {
-		Filter.super.destroy();
 	}
-
-
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain next)
 			throws IOException, ServletException {
 		if (null == request.getCharacterEncoding()) {
-            request.setCharacterEncoding(encoding);
-        }
+			request.setCharacterEncoding(encoding);
+		}
 
-        // Set the default response content type and encoding
-        response.setContentType("text/html; charset=UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        next.doFilter(request, response);
+		response.setContentType("text/html; charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		next.doFilter(request, response);
 	}
 
 }
--- src/net/vadique/khpi/vagonkadb/utils/ConnectionFilter.java
+++ src/net/vadique/khpi/vagonkadb/utils/ConnectionFilter.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -20,44 +21,37 @@ public class ConnectionFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest) request;
-        HttpSession session = req.getSession();
-        UserAccount userInSession = MyUtils.getLoginedUser(session);
-       
-        
+		HttpSession session = req.getSession();
+		UserAccount userInSession = MyUtils.getLoginedUser(session);
+
 		Connection conn = null;
-        try {
-            // Create a Connection.
-        	if (userInSession == null) {
-        		conn = MySQLConnection.getMySQLConnection();
-        	}
-        	else {
-        		conn = MySQLConnection.getMySQLConnection(userInSession.getUsername(), userInSession.getPassword());
-        	}
-            // Set auto commit to false.
-            conn.setAutoCommit(false);
-
-            // Store Connection object in attribute of request.
-            MyUtils.storeConnection(request, conn);
-
-            // Allow request to go forward
-            // (Go to the next filter or target)
-            chain.doFilter(request, response);
-
-            // Invoke the commit() method to complete the transaction with the DB.
-            conn.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-            MySQLConnection.rollbackQuietly(conn);
-            throw new ServletException();
-        } finally {
-        	MySQLConnection.closeQuietly(conn);
-        }
-    }
+		try {
+			if (userInSession == null) {
+				conn = MySQLConnection.getMySQLConnection();
+			} else {
+				conn = MySQLConnection.getMySQLConnection(userInSession.getUsername(), userInSession.getPassword());
+			}
+			
+			conn.setAutoCommit(false);
+			MyUtils.storeConnection(request, conn);
+			chain.doFilter(request, response);
+			conn.commit();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			MySQLConnection.rollbackQuietly(conn);
+			throw new ServletException();
+		} finally {
+			MySQLConnection.closeQuietly(conn);
+		}
+	}
 
 	@Override
 	public void destroy() {
-		Filter.super.destroy();
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
 	}
 
 }
-
