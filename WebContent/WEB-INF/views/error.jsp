2c98fef520abbd081df3c1ad988886420b807562
pixeleon <shevelvaddm@gmail.com>
2020-09-17 09:33:47 +0300

    Added small exception handling

--- WebContent/WEB-INF/views/error.jsp
+++ WebContent/WEB-INF/views/error.jsp
@@ -9,7 +9,7 @@
     <title>Exception</title>
 </head>
 <body>
-<h2>Exception occurred while accessing database</h2>
+<h2>Something went wrong...</h2>
 <p>Type: <%= exception%>
 </p>
 <p>Message: <%= message %>
--- src/net/pixeleon/khpi/vagonkadb/servlet/ReportServlet.java
+++ src/net/pixeleon/khpi/vagonkadb/servlet/ReportServlet.java
@@ -37,7 +37,7 @@ public class ReportServlet extends HttpServlet {
             List<Order> ordersList = DBUtils.selectOrders(MyUtils.getStoredConnection(request));
             ReportUtils.generateReport(ordersList);
             getServletContext().getRequestDispatcher("/orders").forward(request, response);
-        } catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
